7729d752aceb7af81b2ca87f526f9dd02a9c59f6
Favour Adesiyan <Favouradesiyan2@gmail.com>
2024-11-19 19:27:46 -0500

    Initial commit: Add Expense Tracker project files

---
+++ Expense Tracker/Program.cs
@@ -0,0 +1,34 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Dependency Injection for DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+app.Run();
